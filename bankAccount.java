20914d026e893effeda8605be127278271dfdef8
Swavy188 <61544400+Swavy188@users.noreply.github.com>
2020-05-08 00:00:06 +0200

    Create bank account
    Create a method to calculate interesse and print it.

---
+++ bankAccount.java
@@ -0,0 +1,16 @@
+public class Interesse1 {
+
+      public static void main (Strings [] args){
+      
+      double interesse;
+      double capitale;
+      double tasso = 0.025;
+      
+      capitale = 1324;
+      interesse = capitale * tasso;
+      
+      System.out.printnl ("L'interesse dopo un anno è:");
+      System.out.printnl ("interesse");
+      
+      }
+}
